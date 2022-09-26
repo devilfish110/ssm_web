e06845883bd31ee03eec5938d3394cc11bece052
仅此而已 <3563774914@qq.com>
2022-09-26 16:53:30 +0800

    remove ALL

--- README.md
+++
@@ -1,3 +0,0 @@
-###
-
-本代码在学习使用spring/springMVC/mybatis/mybatisPlus时的练习
--- src/main/java/org/study/entities/Result.java
+++
@@ -1,9 +0,0 @@
-package org.study.entities;
-
-import lombok.Data;
-
-@Data
-public class Result {
-    private Integer status;
-    private String message;
-}
